9db9082d156f96cd35d751cfdad49bcf42254876
Sowmya-006 <152410326+Sowmya-006@users.noreply.github.com>
2025-08-10 23:57:12 +0530

    Delete Week-8/GIT directory

--- Week-8/GIT/2. Git-HOL/commands.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-git init
-git status
-# Verify that debug.log and log/ are not tracked
-# Add and commit remaining files
-git add .
-git commit -m "Initial commit ignoring .log files and log folder"
